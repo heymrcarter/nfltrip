5d1af00bbf9171de06f02c374d178abd1fc269a3
Tom Carter <heymrcarter14@gmail.com>
2017-05-28 13:04:48 -0400

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,77 @@
+<template>
+  <div id="app">
+    <aside class="left-rail">
+      <img src="./assets/nfl-logo.jpg" />
+
+      <nav class="navigation">
+        <ul>
+          <li>
+            <router-link to="/">Schedule</router-link>
+
+            <ul>
+              <li><router-link to="/">By week</router-link></li>
+              <li><router-link to="/">By team</router-link></li>
+            </ul>
+          </li>
+          <li><router-link to="/">Trip Planner</router-link></li>
+        </ul>
+      </nav>
+    </aside>
+    <main>
+      <router-view></router-view>
+    </main>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+* { margin: 0; padding: 0; }
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;  
+}
+
+#app::after {
+  content: "";
+  display: table;
+  clear: both;
+}
+
+.left-rail {
+  float: left;
+  width: 15%;
+  height: 100vh;
+  box-shadow: 3px 0px 3px #000;
+  z-index: 100;
+  padding-top: 15px;
+}
+
+.left-rail img {
+  width: 100%;
+}
+
+.left-rail .navigation {
+  text-align: center;
+}
+
+.left-rail .navigation ul { 
+  list-style: none;
+}
+
+main {
+  float: right;
+  width: 85%;
+  height: 100vh;
+  background-color: #f9f9f9;
+  padding: 15px 25px;
+  box-sizing: border-box;
+  z-index: 90;
+}
+</style>
---
+++ src/components/Hello.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="hello">
+    <h1>NFL trip planner</h1>
+    <p class="large">Helps you find the perfect game for a trip!</p>
+    <div class="actions">
+      <router-link to="/" class="btn">View schedule</router-link>
+      <router-link to="/" class="btn">Plan trip</router-link>
+    </div>    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style scoped>
+.hello {
+  box-sizing: border-box;
+}
+
+.hello::after {
+  content: "";
+  display: table;
+  clear: both;
+}
+
+h1, h2 {
+  font-weight: normal;
+  margin-bottom: 10px;
+}
+
+p {
+  margin-bottom: 10px;
+  display: block;
+}
+
+.large { font-size: 18px; }
+
+.actions {
+  margin: 10px 0;
+  box-sizing: border-box;
+}
+
+a {
+  color: #42b983;
+}
+
+.btn {
+  background-color: #fff;
+  padding: 10px;
+  border: 2px solid #013368;
+  border-radius: 10px;
+  color: #013368;
+  text-decoration: none;
+  display: inline-block;
+}
+</style>
